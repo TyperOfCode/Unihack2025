93f4d9559019d0aafc0e1b6c2b81940da1e43254
Ronan Green <ronan.morrissey.green@gmail.com>
2025-03-15 12:12:47 +1100

    Clean Up getRecommendations.py
    Remove unused imports and testing code.

--- backend/getRecommendations.py
+++ backend/getRecommendations.py
@@ -2,11 +2,8 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import json
-from pydantic import BaseModel
-from typing import List, Optional
-
 from models.recommendations import Recommendation, Recommendations
-from models.profile import GiftUserProfile, Preference
+from models.profile import GiftUserProfile
 
 load_dotenv()
 
@@ -30,22 +27,3 @@ def select_gifts(data: GiftUserProfile):
     recommendations_data = json.loads(completion.choices[0].message.content)
     recommendations = [Recommendation(**rec) for rec in recommendations_data]
     return Recommendations(recommendations=recommendations)
-
-
-json_data = GiftUserProfile(**{
-    "Age": 27,
-    "Gender": "Female",
-    "Occupation": "Graphic Designer",
-    "Location": "Seattle, WA",
-    "Relationship_to_Recipient": "Close friend",
-    "likes": [Preference(name="Art", description="Loves impressionist art work", how_important=45), Preference(name="Photography", description="Loves taking photos of her travels and lifestyle", how_important=72), Preference(name="Gaming", description="has been a gamer her whole life and really likes relaxing games like Stardew Valley", how_important=60), Preference(name="Hiking", description="Loves hiking and exploring the outdoors", how_important=80)],
-    "dislikes": [Preference(name="Cooking", description="Hates how time consuming cooking can be", how_important=34)],
-    "Budget": "50",
-    "Gift_Giving_History": ["Painting Set", "Photography Book", "Gaming Mouse", "Hiking Boots"],
-    "Upcoming_Events": ["Birthday", "Christmas", "Anniversary"],
-    "Wishlist": ["Painting Set", "Photography Book", "Gaming Mouse", "Hiking Boots"],
-    "Gift_Guesses": ["Painting Set", "Photography Book", "Gaming Mouse", "Hiking Boots"],
-    "Completed_Percentage": 0.0
-})
-
-print(select_gifts(json_data))
